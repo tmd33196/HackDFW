c75c7882b60ee387c6c7841e79cdfca6781cd329
Lord Horatio <Lord Horatio@Michaels-laptop>
2016-04-14 13:17:53 -0500

    Created Duck class; constructor only so far.

--- HackDFW/src/hackdfw/HackDFW.java
+++ HackDFW/src/hackdfw/HackDFW.java
@@ -10,7 +10,8 @@ public class HackDFW {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
     }
     
