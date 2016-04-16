f3e8809743e895d264b0ba542e66eb06ae531c4c
Tyler Davis <tmd33196@gmail.com>
2016-04-16 12:47:07 -0500

    Added initial play loop

--- HackDFW/src/HackDFW.java
+++ HackDFW/src/HackDFW.java
@@ -2,6 +2,7 @@
 * This is the main file of the program
 */
 
+import java.awt.Point;
 import java.util.Scanner;
 import java.io.IOException;
 
@@ -26,11 +27,24 @@ public class HackDFW
         GoBoard board = new GoBoard(size); //Creates the game board
         Player p1 = new Player(1, size); //Creates player 1 with black pieces
         Player p2 = new Player(2, size); //Creates player 2 with white pieces
+        boolean turn = false;//Who's turn it is, false = player 1, true = player 2
         
         //Infinit play loop
         while(true)
         {
-            
+            Point p;
+            if(!turn)
+            {
+                p = p1.playTurn();
+                System.out.printf("Player 1 puts a piece at: %s", p.toString());
+                board.placeStone(p1.getPlayerNumber(), p);
+            }
+            else
+            {
+                p = p2.playTurn();
+                System.out.printf("Player 2 puts a piece at: %s", p.toString());
+                board.placeStone(p1.getPlayerNumber(), p);
+            }
         }
     }
     
