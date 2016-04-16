ae22abd41fc215de84c98f6c150a4c02d6200e2a
Tyler Davis <tmd33196@gmail.com>
2016-04-16 12:17:25 -0500

    Set up loop to get the size of the board form the use

--- HackDFW/src/HackDFW.java
+++ HackDFW/src/HackDFW.java
@@ -2,12 +2,26 @@
 * This is the main file of the program
 */
 
+import java.util.Scanner;
+import java.io.IOException;
+
 public class HackDFW 
 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
+        Scanner sc = new Scanner(System.in);
+        int size;
         
+        System.out.println("Input the size of the board you would like");
+        
+        while(true)
+        {
+            try{ size = sc.nextInt(); break; }
+            catch(Exception e) { System.out.println("Please enter a number\n"); sc.nextLine(); }
+        }
+        
+        GoBoard board = new GoBoard(size);
     }
     
 }
