1c9a0d568e2ab8f562007940ba924862c174f9ac
Tyler Davis <tmd33196@gmail.com>
2016-04-16 11:05:43 -0500

    Added GoPiece class

---
+++ HackDFW/src/GoPiece.java
@@ -0,0 +1,16 @@
+/*
+ * GoPieces class that will hold the position, color, liberties, and adjacent pieces
+ */
+
+import java.awt.Point;
+
+public class GoPiece
+{
+    
+    private int color;
+    private boolean hasLiberties;
+    private int numLiberties;
+    private Point position;
+    private GoPiece[] adjacent;
+
+}
