863590367df5f997930c398ee3f36626be1c61c6
Tyler Davis <tmd33196@gmail.com>
2016-04-16 15:37:25 -0500

    Minor changes

--- HackDFW/src/HackDFW.java
+++ HackDFW/src/HackDFW.java
@@ -28,54 +28,60 @@ public class HackDFW
         Player[] players = { new Player(1, size), new Player(2, size) }; //The players in the game
         boolean passed = false; //If the last player passed
         boolean gameOver = false; //If the game has ended
+        int player = 0; //Indicates the players turn
         
         //Infinit play loop
         while(!gameOver)
         {
+            //Prints the board
+            System.out.println(board.toString());
+            
+            //Resets the player if it's too high
+            if(player == 2) player = 0;
+            
             Point p; //The point where a player want to put a piece
             
-            //!turn indicates that it is player 1's turn
-            for(int a = 0; a < 2; a ++)
+            //Play the turn, returning the point the player wants to put a piece
+            p = players[player].playTurn();
+
+            //If it is null then the player passes
+            if(p == null)
             {
-                //Play the turn, returning the point the player wants to put a piece
-                p = players[a].playTurn();
-                
-                //If it is null then the player passes
-                if(p == null)
+                //If the last turn was a pass as well then the game is over
+                if(passed == true)
                 {
-                    //If the last turn was a pass as well then the game is over
-                    if(passed == true)
-                    {
-                        gameOver = true;
-                        break;
-                    }
-                    //Else set passed to true to indicate the last turn was a pass
-                    else
-                    {
-                        passed = true;
-                    }
+                    gameOver = true;
+                    break;
                 }
+                //Else set passed to true to indicate the last turn was a pass
                 else
                 {
-                    int val = board.placeStone(players[a].getPlayerNumber(), p);
-                    
-                    if(val == 0)
-                    {
-                        System.out.printf("Player 1 puts a piece at: %s\n", p.toString());
-                        passed = false;
-                    }
-                    else if(val == 1)
-                    {
-                        System.out.println("That space is occupied, enter a new point");
-                        a --;
-                        continue;
-                    }
-                    else if(val == 2)
-                    {
-                        System.out.println("Placing there violates the suicide rule");
-                        a --;
-                        continue;
-                    }
+                    passed = true;
+                }
+            }
+            else
+            {
+                //Place the stone
+                int val = board.placeStone(players[player].getPlayerNumber(), p);
+
+                //Val 0 means the stone was placed successfully
+                if(val == 0)
+                {
+                    System.out.printf("Player 1 puts a piece at: %s\n", p.toString());
+                    passed = false;
+                    player ++;
+                }
+                //Val 1 means the space is occupied
+                else if(val == 1)
+                {
+                    System.out.println("That space is occupied, enter a new point");
+                    continue;
+                }
+                //Val 2 meand the suicide rule was violated
+                else if(val == 2)
+                {
+                    System.out.println("Placing there violates the suicide rule");
+                    continue;
                 }
             }
         }
