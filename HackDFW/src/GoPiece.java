a56b991fdf7435f19ca5820ec09ea9bfa2859a0f
Tyler Davis <tmd33196@gmail.com>
2016-04-16 11:22:51 -0500

    Added getters and setters for GoPieces

--- HackDFW/src/GoPiece.java
+++ HackDFW/src/GoPiece.java
@@ -1,5 +1,6 @@
 /*
  * GoPieces class that will hold the position, color, liberties, and adjacent pieces
+ * Author: Tyler
  */
 
 import java.awt.Point;
@@ -7,10 +8,53 @@ import java.awt.Point;
 public class GoPiece
 {
     
-    private int color;
+    private GoPiece[] adjacent;
+    private Point position;
     private boolean hasLiberties;
     private int numLiberties;
-    private Point position;
-    private GoPiece[] adjacent;
+    private int color;
+    
+    public GoPiece(int _color, Point _position)
+    {
+        color = _color;
+        position = _position;
+    }
+    
+    public Point getPosition()
+    {
+        return position;
+    }
+    
+    public int getColor()
+    {
+        return color;
+    }
+    
+    public void setColor(int _color)
+    {
+        color = _color;
+    }
+    
+    public int getNumLiberties()
+    {
+        return numLiberties;
+    }
+    
+    public void setNumLiberties(int _numLiberties)
+    {
+        numLiberties = _numLiberties;
+        if (numLiberties == 0)
+            setHasLiberties(false);
+    }
+    
+    public void setHasLiberties(boolean _hasLiberties)
+    {
+        hasLiberties = _hasLiberties;
+    }
+    
+    public boolean getHasLiberties()
+    {
+        return hasLiberties;
+    }
 
 }
